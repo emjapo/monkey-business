45a32ab5dc0aa8ccad6cab34d4192c1523dd9593
Emily <porte2@mailbox.winthrop.edu>
2021-10-19 14:32:39 -0400

    finished SimpleObjParse function

--- monkeyBusiness.js
+++ monkeyBusiness.js
@@ -21,4 +21,58 @@ function SimpleObjParse(objFileContents) {
     const faceRE = /^f .*/;
     const textureRE = /^vt .*/;
     const normalRE = /^vn .*/;
+
+    for (let lineIDX=0; lineIDX < objFileLines.length; ++lineIDX) {
+        const line = objFileLines[lineIDX].trim();
+
+        const vertexMatch = vertexRE.exec(line);
+        const faceMatch = faceRE.exec(line);
+        const textureMatch = textureRE.exec(line);
+        const normalMatch = normalRE.exec(line);
+
+        // vertex Line
+        if (vertexMatch != null) {
+            const fields = line.split(/\s/);
+            vertexList.push(    vec4(parseFloat(fields[1]),
+                                    parseFloat(fields[2]),
+                                    parseFloat(fields[3]),
+                                    1.0));
+        }
+
+        // face line
+        else if (faceMatch != null) {
+            const fields = line.split(/\s/);
+
+            var vidxList = new Array();
+            for (let faceIDX = 1; faceIDX < fields.length; ++faceIDX) {
+                var faceVertexIndexStrings = fields[faceIDX].split('/');
+                vidxList.push (parseInt(faceVertexIndexStrings[0]));
+            }
+
+            for (let vidx = 1; vidx < vidxList.length-1; ++vidx) {
+                faceList.push( [vidxList[0]-1, vidxList[vidx]-1, vidxList[vidx + 1]-1 ]);
+            }
+        }
+
+        // texture line
+        else if (textureMatch != null) {
+            const fields = line.split(/\s/);
+            textureList.push(   new Array(parseFloat(fields[1]),
+                                        parseFloat(fields[2])));
+        }
+
+        // normal line
+        else if (normalMatch != null) {
+            const fields = line.split(/\s/);
+            normalList.push(    vec3(parseFloat(fields[1]),
+                                    parseFloat(fields[2]),
+                                    parseFloat(fields[3]) ));
+        }
+
+    }
+    
+    return ({"vertices": vertexList,
+            "faces": faceList,
+            "textures": textureList,
+            "normals": normalList});
 }
